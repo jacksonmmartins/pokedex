e0976719648c7cdd8042640bdfe4d8b5bd611858
jackson <jacksondesobral@hotmail.com>
2023-02-13 10:14:33 -0300

    home_finalizada

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css'
-import Layout from '../pages/components/Layout'
+import Layout from '../../components/Layout'
 
 function App({ Component, pageProps }) {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,42 @@
-
+import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+import Card from '../../components/Card.js'
+
+export async function getStaticProps(){
+  //limitação de pokemons config
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon/' //de onde vai puxar os dados
+
+  //config da chamado dos dados dinamicos
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  // add pokemon index
+  data.results.forEach ((item, index)=>{
+    item.id = index +1
+  })
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+}
 
 
-
-export default function Home() {
+export default function Home({pokemons}) {
   return (
     <>
-      <h1>Pokenext</h1>
+      
+        <div className={styles.title_container}>
+          <h1 className={styles.title}>Poke<span>next</span></h1>
+          <Image src="/images/pokeball.png" width="50" height="50" alt="Pokeball"/>
+        </div>
+      <div className={styles.pokemon_container}>
+        {pokemons.map((pokemon)=>(
+          <Card key={pokemon.id} pokemon={pokemon}/>
+        ))}
+      </div>
+      
     </>
   )
 }
